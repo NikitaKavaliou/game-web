622ee577989efb10cf57d802fecc177b7c09699e
Accu 140 <Accu140@accu.local>
2026-02-02 11:03:00 -0500

    Full rebuild

--- hangman/js/sounds.js
+++ hangman/js/sounds.js
@@ -1,8 +1,8 @@
 export const sounds = {
-  click: new Audio("../sounds/click.wav"),
-  wrong: new Audio("../sounds/wrong.wav"),
-  win: new Audio("../sounds/win.wav"),
-  lose: new Audio("../sounds/lose.wav")
+  click: new Audio("sounds/click.wav"),
+  wrong: new Audio("sounds/wrong.wav"),
+  win: new Audio("sounds/win.wav"),
+  lose: new Audio("sounds/lose.wav")
 };
 
 export function play(sound) {
